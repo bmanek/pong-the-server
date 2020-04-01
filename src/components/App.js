41c7b78e7271d87fad5b695f24ad65b2e3136e75
bmanek <41875460+bmanek@users.noreply.github.com>
2020-04-01 18:40:12 -0400

    add boilerplate/stub App.js & index.js

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class App extends React.Component {
+  state = { resource: "posts"}
+
+  render() {
+    return (
+      <div>
+        Welcome to the Thunderdome
+      </div>
+    )
+  }
+}
+
+export default App
